5df73cf2047ea206d59c1e34830641809dff1cc0
rafasystec <rafasystec@yahoo.com.br>
2019-08-09 21:45:52 -0300

    Creating Treatment, vaccine and login activities.

---
+++ app/src/main/java/client/petmooby/com/br/petmooby/actvity/RegisterUserActivity.kt
@@ -0,0 +1,13 @@
+package client.petmooby.com.br.petmooby.actvity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import client.petmooby.com.br.petmooby.R
+
+class RegisterUserActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register_user)
+    }
+}
