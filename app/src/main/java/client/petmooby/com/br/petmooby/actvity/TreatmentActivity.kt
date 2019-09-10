aea06b6143ce1f126acaf58e075a70859a88c78e
rafasystec <rafasystec@yahoo.com.br>
2019-09-09 21:33:32 -0300

    treatment activity- Update function

--- app/src/main/java/client/petmooby/com/br/petmooby/actvity/TreatmentActivity.kt
+++ app/src/main/java/client/petmooby/com/br/petmooby/actvity/TreatmentActivity.kt
@@ -1,12 +1,16 @@
 package client.petmooby.com.br.petmooby.actvity
 
+import android.app.Activity
 import android.app.ProgressDialog
+import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
+import android.view.View.*
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
+import androidx.annotation.UiThread
 import client.petmooby.com.br.petmooby.R
 import client.petmooby.com.br.petmooby.extensions.setupToolbar
 import client.petmooby.com.br.petmooby.extensions.showAlert
@@ -16,16 +20,20 @@ import client.petmooby.com.br.petmooby.model.enums.EnumTypeInterval
 import client.petmooby.com.br.petmooby.model.enums.EnumTypePeriod
 import client.petmooby.com.br.petmooby.model.enums.EnumTypeTreatment
 import client.petmooby.com.br.petmooby.util.*
+import com.facebook.login.LoginManager
 import kotlinx.android.synthetic.main.activity_treatment.*
+import org.jetbrains.anko.alert
+import org.jetbrains.anko.noButton
 import org.jetbrains.anko.toast
+import org.jetbrains.anko.yesButton
 import java.util.*
 
 
 class TreatmentActivity : BaseActivity() {
 
 //    var dateInformed         = Date()
-    var mDateInitial          = Date()
-    var mDateFinal            = Date()
+    var mDateInitial:Date?          = Date()
+    var mDateFinal:Date?            = Date()
     var currentTreatment: Animal.TreatmentCard?=null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,18 +42,18 @@ class TreatmentActivity : BaseActivity() {
         initSpinners()
 
         edtTreatmentDateInitial.setOnClickListener {
-            DateTimePickerDialog.showDatePicker(this,it,mDateInitial)
+            DateTimePickerDialog.showDatePicker(this,it,mDateInitial!!)
         }
 
         edtTreatmentDateInitial.setOnFocusChangeListener { view, hasFocus -> if(hasFocus)
-            DateTimePickerDialog.showDatePicker(this,view,mDateInitial)
+            DateTimePickerDialog.showDatePicker(this,view,mDateInitial!!)
         }
         edtTreatmentDateFinal.setOnClickListener {
-            DateTimePickerDialog.showDatePicker(this,it,mDateFinal)
+            DateTimePickerDialog.showDatePicker(this,it,mDateFinal!!)
         }
 
         edtTreatmentDateFinal.setOnFocusChangeListener { view, hasFocus -> if(hasFocus)
-            DateTimePickerDialog.showDatePicker(this,view,mDateFinal)
+            DateTimePickerDialog.showDatePicker(this,view,mDateFinal!!)
         }
 
         var treatmentID = intent.getLongExtra(Parameters.TREATMENT,0L)
@@ -72,30 +80,50 @@ class TreatmentActivity : BaseActivity() {
             edtTreatmentDesc.setText(currentTreatment?.name)
             swtTreatmentAlarm.isChecked = currentTreatment?.isActive!!
             edtTreatmentNote.setText(currentTreatment?.notes)
-            spTreatmentInterval.setSelection((currentTreatment?.timeInterval!! - 1).toInt())
+            //spTreatmentInterval.setSelection((currentTreatment?.timeInterval!! - 1).toInt())
             spTreatmentTypeInterval.setSelection(currentTreatment?.typeInterval?.ordinal!!)
             spTreatmentPeriod.setSelection(currentTreatment?.typePeriod?.ordinal!!)
             spTreatmentType.setSelection(currentTreatment?.typeTreatment?.ordinal!!)
-
+            spTreatmentType.isEnabled = false
+            edtTreatmentInterval.setText(currentTreatment?.timeInterval!!.toString())
 
         }
     }
 
     private fun initSpinners(){
         spTreatmentType.adapter = ArrayAdapter<EnumTypeTreatment>(this, LayoutResourceUtil.getSpinnerDropDown(),EnumTypeTreatment.values())
-        spTreatmentType.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+//        spTreatmentType.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+//            override fun onNothingSelected(parent: AdapterView<*>?) {
+//
+//            }
+//
+//            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+//
+//            }
+//
+//        }
+        //spTreatmentInterval.adapter     = ArrayAdapter<Int>(this,LayoutResourceUtil.getSpinnerDropDown(), listOf(1,2,3,4,5,6,7,8,9,10))
+        spTreatmentPeriod.adapter       = ArrayAdapter<EnumTypePeriod>(this,LayoutResourceUtil.getSpinnerDropDown(),EnumTypePeriod.values())
+        spTreatmentPeriod.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
             override fun onNothingSelected(parent: AdapterView<*>?) {
 
             }
 
             override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
-
+                when(position){
+                    EnumTypePeriod.ALWAYS.ordinal ->{
+                        //Only need to inform the initial date
+                        setFinalDateVisibility(false)
+                    }
+                    EnumTypePeriod.INFORMED.ordinal ->{
+                        setFinalDateVisibility(true)
+                    }
+                }
             }
 
         }
-        spTreatmentInterval.adapter     = ArrayAdapter<Int>(this,LayoutResourceUtil.getSpinnerDropDown(), listOf(1,2,3,4,5,6,7,8,9,10))
-        spTreatmentPeriod.adapter       = ArrayAdapter<EnumTypePeriod>(this,LayoutResourceUtil.getSpinnerDropDown(),EnumTypePeriod.values())
         spTreatmentTypeInterval.adapter = ArrayAdapter<EnumTypeInterval>(this,LayoutResourceUtil.getSpinnerDropDown(), EnumTypeInterval.values())
+
     }
 
     private fun validate() : Boolean{
@@ -114,9 +142,14 @@ class TreatmentActivity : BaseActivity() {
             showAlert(R.string.pleaseSelectATreatmentType)
             return false
         }
-        if(spTreatmentInterval.selectedItem == null){
+        if(edtTreatmentInterval.text.toString().isEmpty()){
             showAlert(R.string.pleaseSelectATreatmentInterval)
             return false
+        }else{
+            val interval = edtTreatmentInterval.text.toString().toLong()
+            if(interval <= 0){
+                showAlert(R.string.pleaseIntervalShouldGreatThenZero)
+            }
         }
 
         if(spTreatmentPeriod.selectedItem == null){
@@ -127,6 +160,18 @@ class TreatmentActivity : BaseActivity() {
             edtTreatmentDesc.error = getString(R.string.pleaseGiveATreatmentName)
             edtTreatmentDesc.requestFocus()
             return false
+        }
+
+        when(spTreatmentPeriod.selectedItem as EnumTypePeriod){
+            EnumTypePeriod.INFORMED ->{
+                if(!mDateFinal!!.after(mDateInitial)){
+                    showAlert(R.string.pleaseFinalDateShoulBeGreateThanInitial)
+                }
+            }
+            EnumTypePeriod.ALWAYS ->{
+                mDateFinal = null
+
+            }
         }
 
         return true
@@ -145,7 +190,7 @@ class TreatmentActivity : BaseActivity() {
                 identity            = Random().nextInt(1000000000).toLong()
                 isActive            = swtTreatmentAlarm.isChecked
                 notes               = edtTreatmentNote.text.toString()
-                timeInterval        = (spTreatmentInterval.selectedItem as Int).toLong()
+                timeInterval        = edtTreatmentInterval.text.toString().toLong()
                 typeInterval        = spTreatmentTypeInterval.selectedItem as EnumTypeInterval
                 typePeriod          = spTreatmentPeriod.selectedItem as EnumTypePeriod
                 typeTreatment       = spTreatmentType.selectedItem as EnumTypeTreatment
@@ -160,7 +205,7 @@ class TreatmentActivity : BaseActivity() {
                 name               = edtTreatmentDesc.text.toString()
                 isActive           = swtTreatmentAlarm.isChecked
                 notes           = edtTreatmentNote.text.toString()
-                timeInterval    = (spTreatmentInterval.selectedItem as Int).toLong()
+                timeInterval    = edtTreatmentInterval.text.toString().toLong()
                 typeInterval    = spTreatmentTypeInterval.selectedItem as EnumTypeInterval
                 typePeriod      = spTreatmentPeriod.selectedItem as EnumTypePeriod
                 typeTreatment   = spTreatmentType.selectedItem as EnumTypeTreatment
@@ -175,9 +220,13 @@ class TreatmentActivity : BaseActivity() {
                 .addOnSuccessListener {
                     if(isDelete){
                         toast(R.string.treatmentDeleted)
+                        var intent = Intent()
+                        //intent.putExtra()
+                        setResult(Activity.RESULT_OK, intent)
                         finish()
                     }else {
                         saveAndSetResult(dialog)
+                        spTreatmentType.isEnabled = false
                     }
                 }.addOnFailureListener {
                     showAlert(R.string.wasNotPossibleSaveVaccine)
@@ -197,17 +246,42 @@ class TreatmentActivity : BaseActivity() {
     override fun onOptionsItemSelected(item: MenuItem?): Boolean {
         when(item?.itemId){
             R.id.menuSave ->{
-//               if(currentTreatment == null){
-//                   save()
-//               }else{
-//                   save()
-//               }
                 save()
             }
             R.id.menuDelete ->{
+                alert(R.string.areYouSureToDeleteThisTreatment,R.string.delete) {
+                    yesButton {  delete() }
+                    noButton {it.dismiss()}
+                }.show()
 
             }
         }
         return super.onOptionsItemSelected(item)
+    }
+
+    private fun delete(){
+        var dialog = showLoadingDialog()
+        with(VariablesUtil.gbSelectedAnimal?.treatmentCard?.filter { it.identity == currentTreatment?.identity }!![0]){
+            VariablesUtil.gbSelectedAnimal?.treatmentCard?.remove(this)
+        }
+        saveAnimal(dialog,true)
+    }
+
+    override fun onBackPressed() {
+        super.onBackPressed()
+        setResult(Activity.RESULT_OK)
+        finish()
+    }
+    @UiThread
+    fun setFinalDateVisibility(isVisible: Boolean){
+        if(isVisible){
+            edtTreatmentDateFinal.isEnabled     = true
+            edtTreatmentDateFinal.visibility    = VISIBLE
+            ivTreatment6.visibility             = VISIBLE
+        }else {
+            edtTreatmentDateFinal.isEnabled     = false
+            edtTreatmentDateFinal.visibility    = INVISIBLE
+            ivTreatment6.visibility             = INVISIBLE
+        }
     }
 }
--- app/src/main/java/client/petmooby/com/br/petmooby/actvity/TreatmentListActivity.kt
+++ app/src/main/java/client/petmooby/com/br/petmooby/actvity/TreatmentListActivity.kt
@@ -1,11 +1,9 @@
 package client.petmooby.com.br.petmooby.actvity
 
-import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
-import androidx.recyclerview.widget.GridLayoutManager
 import client.petmooby.com.br.petmooby.R
 import client.petmooby.com.br.petmooby.adapter.TreatmentListAdapter
 import client.petmooby.com.br.petmooby.extensions.getDefaultLayoutManager
@@ -15,26 +13,14 @@ import client.petmooby.com.br.petmooby.util.Parameters
 import client.petmooby.com.br.petmooby.util.ResultCodes
 import client.petmooby.com.br.petmooby.util.VariablesUtil
 import kotlinx.android.synthetic.main.activity_treatment_list.*
-//import org.parceler.Parcels
-
-//import org.parceler.Parcels
 
 class TreatmentListActivity : BaseActivity() {
-
-//    private var treatments = mutableListOf<Animal.TreatmentCard>()
     private var adapter:TreatmentListAdapter?= null
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_treatment_list)
         setupToolbar(R.id.toolbarTreatmentList, R.string.treatments)
         initRcViewList()
-//        var animal = intent.getParcelableExtra<Animal>(Parameters.ANIMAL_PARAMETER)
-//        var animal = intent.getSerializableExtra(Parameters.ANIMAL_PARAMETER) as Animal
-//        var animal = Parcels.unwrap<Animal>(intent.getParcelableExtra(Parameters.ANIMAL_PARAMETER))
-//        for(treatment in VariablesUtil.gbSelectedAnimal?.treatmentCard!!){
-//            addTreatment(treatment)
-//        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -64,29 +50,16 @@ class TreatmentListActivity : BaseActivity() {
         }
     }
 
-    private fun addTreatment(treatmentCard: Animal.TreatmentCard){
-        VariablesUtil.gbSelectedAnimal?.treatmentCard?.add(treatmentCard)
-        adapter?.notifyItemInserted(VariablesUtil.gbSelectedAnimal?.treatmentCard?.lastIndex!!)
-    }
-
     private fun onTreatmentClick(treatmentCard: Animal.TreatmentCard){
         val intent = Intent(this,TreatmentActivity::class.java)
         intent.putExtra(Parameters.ACTION,ResultCodes.REQUEST_UPDATE)
         intent.putExtra(Parameters.TREATMENT,treatmentCard.identity)
-//        intent.putExtra(Parameters.TREATMENT,treatmentCard)
-//        intent.putExtra(Parameters.TREATMENT, Parcels.wrap(treatmentCard))
         startActivityForResult(intent,ResultCodes.REQUEST_UPDATE)
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-        if(resultCode == Activity.RESULT_OK){
-            if(requestCode == ResultCodes.REQUEST_UPDATE){
-                val treatment = data?.getSerializableExtra(Parameters.TREATMENT) as Animal.TreatmentCard
-//                val treatment = Parcels.unwrap<Animal.TreatmentCard>(data?.getParcelableExtra(Parameters.TREATMENT))
-                addTreatment(treatment)
-            }
-        }
+        rcViewTreatmentList.adapter?.notifyDataSetChanged()
     }
 
 
